320b87b0740af749a4e94707d3f4657e17c95c18
Tomas <tomascarrerasalbesa@gmail.com>
2025-11-08 20:09:49 +0100

    sprint-3 con css Juego Piedra Papel Tijera

--- js/script.js
+++ js/script.js
@@ -7,34 +7,36 @@ let puntosUsuario = 0;
 let puntosOrdenador = 0;
 
 botonJugada.forEach((boton) => {
-    boton.addEventListener('click', () => {
-        let jugadaUsuario = boton.dataset.jugada
-        jugadaConsola(jugadaUsuario)
-    });
-})
+    boton.addEventListener('click', () => jugar(boton.dataset.jugada));
+});
+
+
 
 console.log(botonJugada);
 
 const opciones = ['piedra', 'papel', 'tijera'];
 
-function jugadaConsola(eleccion) {
-    const makina = Math.floor(Math.random() * opciones.length);
-    const jugadaMaquina = opciones[makina];
+function jugar(jugadaUsuario) {
+    const jugadaMaquina = opciones[Math.floor(Math.random() * opciones.length)];
     let resultado = '';
-    if (eleccion === jugadaMaquina) {
+    if (jugadaUsuario === jugadaMaquina) {
         resultado = 'Empate';
-    } else if (eleccion === 'piedra' && makina === 'papel' ||
-        eleccion === 'tijera' && makina === 'piedra' ||
-        eleccion === 'papel' && makina === 'tijera') {
-        resultado = 'Has Ganado'
+    } else if (jugadaUsuario === 'piedra' && jugadaMaquina === 'tijera' ||
+        jugadaUsuario === 'tijera' && jugadaMaquina === 'papel' ||
+        jugadaUsuario === 'papel' && jugadaMaquina === 'piedra') {
+        resultado = 'HAS GANADO 😆'
         puntosUsuario++
     } else {
-        resultado = 'Has Perdido';
+        resultado = 'HAS PERDIDO 😒'
         puntosOrdenador++
     }
-    resultados.innerText = resultado
     contadorOrdenador.innerText = `Puntos del ordenador ${puntosOrdenador}`;
     contadorUsuario.innerText = `Puntos del Usuario ${puntosUsuario}`;
+    resultados.innerText = resultado;
+    resultados.style.animation = 'none';
+    resultados.offsetHeight;
+    resultados.style.animation = 'resultado 3s linear';
 }
+
 
 
